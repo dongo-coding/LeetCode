d8b7b2fd94d03df064b5daf665a61df5de582d48
Do Ngo <bahuynh619@gmail.com>
2025-09-26 12:41:43 +0700

    Create Valid_Parentheses.cpp

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for(int i=0;i<s.length();i++){
+            if (s[i] == '(') st.push(s[i]);
+            else if (s[i] == '{') st.push(s[i]);
+            else if (s[i] == '[') st.push(s[i]);
+            else {
+                if (st.empty()) return false;
+                char t = st.top();
+                st.pop();
+                if (s[i] == ')' && t != '(' ) return false;
+                else if (s[i] == '}' && t != '{') return false;
+                else if (s[i] == ']' && t != '[') return false;
+            }
+        }
+        return st.empty();
+    }
+};
